8ac08a5c8903bf6dc5ce1c5f23b56cbc116b5218
jtfogarty <jeff@fogarty.org>
2024-01-16 18:50:47 -0600

    contact

---
+++ src/lib/components/CardEmail.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import { Input } from "$lib/components/ui/input";
+  import { Label } from "$lib/components/ui/label";
+</script>
+
+<div class="grid grid-cols-2 gap-4">
+  <div class="space-y-1">
+    <Label for="name">Name</Label>
+    <Input id="name" placeholder="name" />
+  </div>
+  <div class="space-y-1">
+    <Label for="email">Email</Label>
+    <Input id="email" type="email" placeholder="email" />
+  </div>
+  <div class="space-y-1">
+    <Label for="pn">Phone Number</Label>
+    <Input id="pn-01" placeholder="###-###-####" />
+  </div>
+  <div class="space-y-1">
+    <Label for="cn">Company/Organization Name</Label>
+    <Input id="coname" placeholder="Company/Organization Name" />
+  </div>
+  <div class="space-y-1">
+    <Label for="an">Street Address</Label>
+    <Input id="addr" placeholder="" />
+  </div>
+  <div class="space-y-1">
+    <Label for="city">City</Label>
+    <Input id="city" placeholder="" />
+  </div>
+  <div class="space-y-1">
+    <Label for="state">State</Label>
+    <Input id="state" placeholder="" />
+  </div>
+  <div class="space-y-1">
+    <Label for="zip">Zip Code</Label>
+    <Input id="zip" placeholder="" />
+  </div>
+</div>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -6,6 +6,7 @@
   import { Input } from "$lib/components/ui/input";
   import { Label } from "$lib/components/ui/label";
   import { Textarea } from "$lib/components/ui/textarea";
+	import CardEmail from "$lib/components/CardEmail.svelte";
 
   const choice = [
     { value: "customer", label: "I am already a customer" },
@@ -30,40 +31,7 @@
         </Card.Description>
       </Card.Header>
       <Card.Content class="space-y-2 w-[800px]">
-        <div class="grid grid-cols-2 gap-4">
-          <div class="space-y-1">
-            <Label for="name">Name</Label>
-            <Input id="name" placeholder="name" />
-          </div>
-          <div class="space-y-1">
-            <Label for="email">Email</Label>
-            <Input id="email" type="email" placeholder="email" />
-          </div>
-          <div class="space-y-1">
-            <Label for="pn">Phone Number</Label>
-            <Input id="pn-01" placeholder="###-###-####" />
-          </div>
-          <div class="space-y-1">
-            <Label for="cn">Company/Organization Name</Label>
-            <Input id="coname" placeholder="Company/Organization Name" />
-          </div>
-          <div class="space-y-1">
-            <Label for="an">Street Address</Label>
-            <Input id="addr" placeholder="" />
-          </div>
-          <div class="space-y-1">
-            <Label for="city">City</Label>
-            <Input id="city" placeholder="" />
-          </div>
-          <div class="space-y-1">
-            <Label for="state">State</Label>
-            <Input id="state" placeholder="" />
-          </div>
-          <div class="space-y-1">
-            <Label for="zip">Zip Code</Label>
-            <Input id="zip" placeholder="" />
-          </div>
-        </div>
+        <CardEmail />
         <Textarea placeholder="Type your message here." />
         <Select.Root>
           <Select.Trigger class="w-[300px]">
@@ -96,14 +64,7 @@
         </Card.Description>
       </Card.Header>
       <Card.Content class="space-y-2">
-        <div class="space-y-1">
-          <Label for="current">Current service</Label>
-          <Input id="current" type="service" />
-        </div>
-        <div class="space-y-1">
-          <Label for="new">New service</Label>
-          <Input id="new" type="service" />
-        </div>
+        <CardEmail />
       </Card.Content>
       <Card.Footer>
         <Button>Submit</Button>
